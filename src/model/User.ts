a72677e043534f18c2b6774668f7fd5d9a7de893
Deval <devalujeniya@gmail.com>
2025-07-22 17:36:43 +0530

    update usemodel

--- src/model/User.ts
+++ src/model/User.ts
@@ -66,6 +66,6 @@ const UserSchema: Schema<User> = new Schema({
     messages: [messageSchema]
 })
 
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("Use", UserSchema)
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
 
 export default UserModel;
--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,13 +1,13 @@
 import {z} from 'zod';
 
-export const userValidation = z
+export const usernameValidation = z
     .string()
     .min(4,"Username must be at least 4 characters long")
     .max(20, "Username must be at most 20 characters long")
     .regex(/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores");
 
 export const signUpSchema = z.object({
-    username: userValidation,
+    username: usernameValidation,
     email: z.email({message: "Invalid email address"}),
-    password: z.string().min(8, {message: "Password must be at least 8 characters long"})
+    password: z.string().min(6, {message: "Password must be at least 6 characters long"})
 })
